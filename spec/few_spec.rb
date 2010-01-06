b99f6378df34cac82be075d311dbd5e292bbc6ff
ujihisa <ujihisa@gmail.com>
2010-01-06 15:29:37 -0800

    Specs of Few Command Line Interface

--- spec/few_spec.rb
+++ spec/few_spec.rb
@@ -1,8 +1,31 @@
-describe 'few' do
-  it 'is awesome' do
-    def open_browser(path); path; end
-    def ARGF.read; 'a'; end
-    def ARGF.filename; 'b'; end
-    few.should be_an_instance_of(String)
+describe 'few command' do
+  before do
+    @few = File.dirname(__FILE__) + '/../bin/few'
+  end
+
+  it 'shows help with -h option' do
+    `#{@few} -h`.should match(/USAGE/)
+  end
+
+  it 'shows version with -v option' do
+    `#{@few} -v`.should match(/\d\.\d\.\d/)
+  end
+
+  it 'updates itself with --selfupdate option' do
+    pending
+  end
+
+  it 'opens your browswer the HTML flavored data given by pipe' do
+    # $ cat FILE | few
+    pending
+  end
+
+  it 'opens your browswer the HTML flavored data given by parameter' do
+    # $ few FILE
+    pending
+  end
+
+  it 'also accepts --filetype= option' do
+    pending # based on the last specification
   end
 end
