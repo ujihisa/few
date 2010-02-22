e8591e6aa40756e550906670910316da3196b356
sorah <sora134@gmail.com>
2010-02-22 21:27:47 +0900

    Fix some

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -1,23 +1,23 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-describe FewConfig do
-  it 'new is private method' do
-    lambda {FewConfig.new}.should raise_error(NoMethodError)
+describe Few::Config do
+  before(:all) do
+    @config = Few::Config.new
   end
   
   it 'can store with self[]=' do
-    FewConfig[:a] = 1
+    Few::Config[:a] = 1
   end
 
   it 'can read with self[]' do
-    FewConfig[:a].should == 1
+    Few::Config[:a].should == 1
   end
   
   it 'can store with self.<name>=' do
-    FewConfig.b = 2
+    Few::Config.b = 2
   end
 
   it 'can read with self.<name>' do
-    FewConfig.b.should == 2
+    Few::Config.b.should == 2
   end
 end
