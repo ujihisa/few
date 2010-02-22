17291c77b35bcbc0282f3bbe103c41d74b8896cc
sorah <sora134@gmail.com>
2010-02-22 22:28:19 +0900

    Implement Few::Config

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -4,20 +4,12 @@ describe Few::Config do
   before(:all) do
     @config = Few::Config.new
   end
-  
-  it 'can store with self[]=' do
-    Few::Config[:a] = 1
-  end
 
-  it 'can read with self[]' do
-    Few::Config[:a].should == 1
-  end
-  
   it 'can store with self.<name>=' do
-    Few::Config.b = 2
+    @config.b = 2
   end
 
   it 'can read with self.<name>' do
-    Few::Config.b.should == 2
+    @config.b.should == 2
   end
 end
